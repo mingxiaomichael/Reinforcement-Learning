22c68f70adb8f0a7cc321658a8125a33ce8a546a
MingxiaoLi <lmx@umich.edu>
2023-06-17 18:54:50 -0400

    add policy iteration - simple version :)

---
+++ Algorithm/PolicyIteration/FrozenLake/PolicyIteration.py
@@ -0,0 +1,16 @@
+
+# env = gym.make('FrozenLake-v0')
+# env = gym.make("FrozenLake-v1", render_mode="human", is_slippery=False)
+# print(env.action_space.n)
+# print(env.observation_space.n)
+# print(env.P)
+# env.reset()
+# env.render()
+# a = [2, 2, 1, 1, 1, 2]
+
+# for t in a:
+#     env.render()
+#     # action = env.action_space.sample()
+#     action = t
+#     new_state, reward, is_truncated, is_finished, prob = env.step(action)
+#     print(new_state, reward, is_truncated, is_finished)
