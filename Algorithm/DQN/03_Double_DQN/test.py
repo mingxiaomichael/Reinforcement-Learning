7a12b063feaa0225d4b79bedfc8ca6298eaab208
MingxiaoLi <lmx@umich.edu>
2023-08-10 23:39:49 -0400

    create Double DQN :)

---
+++ Algorithm/DQN/03_Double_DQN/test.py
@@ -0,0 +1,28 @@
+import gymnasium as gym
+import numpy as np
+
+env = gym.make('Pendulum-v1', g=9.81)
+print("action_space: ", env.action_space.n)
+print("observation_space: ", env.observation_space.n)
+# max_episodes = 1
+# max_steps = 100
+# scores = []
+# actions = range(env.action_space.n)
+# for i in range(1, max_episodes + 1):
+#     state = env.reset()
+#     score = 0
+#     for j in range(max_steps):
+#         env.render()
+#         action = np.random.choice(actions)
+#         state, reward, done, _, _ = env.step(action)
+#         env.render()
+#         score += reward
+#         print(j, score, reward, done)
+#         if done:
+#             # print(state, reward, done)
+#             # done == True, indicates reach the terminal goal or the truncated state
+#             # how to solve: making the score higher before done == True
+#             if reward != -100:
+#                 print(state, reward, done)
+
+    # scores.append(score)
