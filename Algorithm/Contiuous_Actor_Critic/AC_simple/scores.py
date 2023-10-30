23b86ce548e6877eb1f48057b90c34a4f15e8ada
MingxiaoLi <lmx@umich.edu>
2023-10-30 15:29:05 -0400

    finish DDPG

--- Algorithm/Contiuous_Actor_Critic/AC_simple/scores.py
+++ Algorithm/Contiuous_Actor_Critic/AC_simple/scores.py
@@ -1,13 +1,20 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 data = []
+avg = []
+ten = np.zeros(10)
 with open('scores.txt', 'r') as file:
     for line in file:
         data.append(float(line.strip()))
+for i in range(len(data)):
+    ten[i % 10] = data[i]
+    avg.append(np.mean(ten))
 
-plt.plot(data)
+
+plt.plot(avg)
 plt.xlabel('episodes')
 plt.ylabel('score')
 
-plt.grid(True)
+# plt.grid(True)
 plt.show()
